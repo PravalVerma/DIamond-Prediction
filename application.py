f4078507263c38a0552e17dce2a959cf80bb98f7
PravalVerma <pravalverma2003@gmail.com>
2026-02-04 03:15:12 +0530

    Updated requirements.txt and added runtime.txt for the deployment on render

---
+++ application.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, render_template, jsonify
+from flask_cors import cross_origin
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+import os
+
+application = Flask(__name__)
+app = application
+
+@app.route('/')
+@cross_origin()
+def home_page():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+@cross_origin()
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    data = CustomData(
+        carat=float(request.form.get('carat')),
+        depth=float(request.form.get('depth')),
+        table=float(request.form.get('table')),
+        x=float(request.form.get('x')),
+        y=float(request.form.get('y')),
+        z=float(request.form.get('z')),
+        cut=request.form.get('cut'),
+        color=request.form.get('color'),
+        clarity=request.form.get('clarity')
+    )
+
+    pred_df = data.get_data_as_dataframe()
+    predict_pipeline = PredictPipeline()
+    pred = predict_pipeline.predict(pred_df)
+
+    results = round(pred[0], 2)
+    return render_template('index.html', results=results)
+
+@app.route('/predictAPI', methods=['POST'])
+@cross_origin()
+def predict_api():
+    data = CustomData(
+        carat=float(request.json['carat']),
+        depth=float(request.json['depth']),
+        table=float(request.json['table']),
+        x=float(request.json['x']),
+        y=float(request.json['y']),
+        z=float(request.json['z']),
+        cut=request.json['cut'],
+        color=request.json['color'],
+        clarity=request.json['clarity']
+    )
+
+    pred_df = data.get_data_as_dataframe()
+    predict_pipeline = PredictPipeline()
+    pred = predict_pipeline.predict(pred_df)
+
+    return jsonify({"price": round(pred[0], 2)})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
